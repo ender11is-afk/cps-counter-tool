1107bb0843eb1e762e7ded1eff638c4ef6077f66
ender11is-afk <66556826+ender11is-afk@users.noreply.github.com>
2021-02-02 19:53:47 +0700

    cool
    just created this

---
+++ localscript.lua
@@ -0,0 +1,28 @@
+-- Clicks Counter.
+-- Presented to you, by 1559EnderAlt or ender11.
+
+local tool = script.Parent
+local clicks = 0
+
+local canClick = false
+
+function reset() -- idk lifesaver ig
+	clicks = 0
+end
+
+tool.Equipped:Connect(function()
+	canClick = true
+end)
+
+tool.Unequipped:Connect(function()
+	canClick = false
+	if clicks>0 and clicks~=0 then reset() end
+end)
+
+tool.Activated:Connect(function(add)
+	add = 1
+	if clicks==0 then
+		clicks+=add
+		print(clicks)
+	end
+end)
